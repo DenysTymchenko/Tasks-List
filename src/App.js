ceb245f075736f378073ef646cac0e793657b985
Denys <tymchenko.denys@gmail.com>
2023-05-26 02:59:16 +0300

    bug with task loading fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
+import { getTasks } from './store/modules/tasks/reducer';
 import TaskList from './pages/TaskList/TaskList';
-import getTasks from './store/modules/tasks/reducer';
 import './App.css';
 
 function App() {
