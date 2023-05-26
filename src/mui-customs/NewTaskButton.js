1130015faee523a92cd7f87f832497c13ff29ac7
Denys <tymchenko.denys@gmail.com>
2023-05-26 20:35:31 +0300

    NewTaskButton displayed properly now

--- src/mui-customs/NewTaskButton.js
+++ src/mui-customs/NewTaskButton.js
@@ -2,7 +2,7 @@ import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 
 export const NewTaskButton = styled(Button)(({ theme }) => ({
-  position: 'absolute',
+  position: 'fixed',
   bottom: '10px',
   right: '10px',
   color: 'var(--secondary-alt)',
