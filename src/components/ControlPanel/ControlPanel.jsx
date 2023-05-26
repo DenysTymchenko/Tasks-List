349bbaa24a0b2463bc8e0dce21be8ec43f0eb172
Denys <tymchenko.denys@gmail.com>
2023-05-26 17:07:47 +0300

    ControlPanel base created

---
+++ src/components/ControlPanel/ControlPanel.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { Container, InputLabel, MenuItem, FormControl, Select } from "@mui/material";
+import { SearchBar } from "../../mui-customs/SearchBar";
+import './ControlPanel.css';
+
+export default function ControlPanel() {
+  const [status, setStatus] = useState('des');
+  const [priority, setPriority] = useState('des');
+  const [sort, setSort] = useState('priority');
+
+  const handleChange = (e, setChange) => {
+    setChange(e.target.value);
+  };
+
+  return (
+    <Container className='control-panel'>
+      <SearchBar
+        label="Find task"
+        placeholder="Enter it's title"
+        variant="outlined"
+      //onChange={(e) => handleQuery(e.target.value)}
+      />
+      <FormControl>
+        <InputLabel>Status</InputLabel>
+        <Select
+          value={status}
+          label='Status'
+          onChange={(e) => handleChange(e, setStatus)}
+        >
+          <MenuItem value='asc'>Ascending</MenuItem>
+          <MenuItem value='des'>Descending</MenuItem>
+        </Select>
+      </FormControl>
+
+      <FormControl>
+        <InputLabel>Priority</InputLabel>
+        <Select
+          value={priority}
+          label='Priority'
+          onChange={(e) => handleChange(e, setPriority)}
+        >
+          <MenuItem value='asc'>Ascending</MenuItem>
+          <MenuItem value='des'>Descending</MenuItem>
+        </Select>
+      </FormControl>
+
+      <FormControl>
+        <InputLabel>Sort by</InputLabel>
+        <Select
+          value={sort}
+          label='Sort by'
+          onChange={(e) => handleChange(e, setSort)}
+        >
+          <MenuItem value='title'>Title</MenuItem>
+          <MenuItem value='priority'>Priority</MenuItem>
+          <MenuItem value='status'>Status</MenuItem>
+        </Select>
+      </FormControl>
+    </Container>
+  )
+}
--- src/pages/TaskList/TaskList.jsx
+++ src/pages/TaskList/TaskList.jsx
@@ -1,9 +1,11 @@
 import { useSelector } from 'react-redux';
 import { Container } from '@mui/material';
 import CreateNewTask from '../../components/CreateNewTask/CreateNewTask';
-import './TaskList.css';
+import { SearchBar } from '../../mui-customs/SearchBar';
 import CompletedTasks from '../../components/CompletedTasks/CompletedTasks';
 import UncompletedTasks from '../../components/UncompletedTasks/UncompletedTasks';
+import './TaskList.css';
+import ControlPanel from '../../components/ControlPanel/ControlPanel';
 
 export default function TaskList() {
   const { tasks } = useSelector((state) => state.tasksReducer);
@@ -11,10 +13,13 @@ export default function TaskList() {
   const completedTasks = tasks.filter(task => task.completed);
 
   return (
-    <Container className='task-list'>
-      <UncompletedTasks uncompletedTasks={uncompletedTasks} />
-      <CompletedTasks  completedTasks={completedTasks}/>
-      <CreateNewTask />
-    </Container>
+    <>
+      <ControlPanel />
+      <Container className='task-list'>
+        <UncompletedTasks uncompletedTasks={uncompletedTasks} />
+        <CompletedTasks completedTasks={completedTasks} />
+        <CreateNewTask />
+      </Container>
+    </>
   );
 }
